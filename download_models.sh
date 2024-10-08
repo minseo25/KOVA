c706eb2c114425f0ef6fd44d079af7a6d1ceab7e
Min seo Kim <minseokim@Minui-MacBookAir.local>
2024-10-08 15:35:55 +0900

    add: clipboard usage, change UI, add VLM support

--- download_models.sh
+++ download_models.sh
@@ -2,16 +2,13 @@
 
 mkdir -p models
 
-echo "Downloading gemma-2-2b-it..."
-huggingface-cli download google/gemma-2-2b-it --local-dir="models/gemma-2-2b-it"
-
-echo "Downloading llama-3-Korean-Bllossom-8B-Q4_K_M..."
-huggingface-cli download MLP-KTLim/llama-3-Korean-Bllossom-8B-gguf-Q4_K_M --local-dir="models/llama-3-korean-bllossom-8b-gguf"
+echo "Downloading qwen2.5-1.5b-instruct..."
+huggingface-cli download Qwen/Qwen2.5-1.5B-Instruct --local-dir="models/qwen2.5-1.5b"
 
 echo "Downloading pyannote/segmentation-3.0..."
 huggingface-cli download pyannote/segmentation-3.0 --local-dir="models/pyannote-segmentation-3"
 
-echo "Downloading faster-whisper-large-v3..."
-huggingface-cli download Systran/faster-whisper-small --local-dir="models/faster-whisper-small"
+echo "Downloading pyannote/segmentation..."
+huggingface-cli download pyannote/segmentation --local-dir="models/pyannote-segmentation"
 
 echo "Downloads completed."
